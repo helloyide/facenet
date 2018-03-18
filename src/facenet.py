fix bug about precision and recall calculation

src/facenet.py
@@ -492,8 +492,9 @@ def calculate_roc(thresholds, embeddings1, embeddings2, actual_issame, nrof_fold
 
     tprs = np.zeros((nrof_folds, nrof_thresholds))
     fprs = np.zeros((nrof_folds, nrof_thresholds))
-    ppvs = np.zeros((nrof_folds, nrof_thresholds))
     accuracy = np.zeros((nrof_folds))
+    precision = np.zeros((nrof_folds))
+    recall = np.zeros((nrof_folds))
 
     diff = np.subtract(embeddings1, embeddings2)
     dist = np.sum(np.square(diff), 1)
@@ -507,13 +508,13 @@ def calculate_roc(thresholds, embeddings1, embeddings2, actual_issame, nrof_fold
             _, _, _, acc_train[threshold_idx] = calculate_accuracy(threshold, dist[train_set], actual_issame[train_set])
         best_threshold_index = np.argmax(acc_train)
         for threshold_idx, threshold in enumerate(thresholds):
-            tprs[fold_idx, threshold_idx], fprs[fold_idx, threshold_idx], ppvs[fold_idx, threshold_idx], _ = calculate_accuracy(threshold,
-                                                                                                 dist[test_set],
-                                                                                                 actual_issame[
-                                                                                                     test_set])
-        _, _, _, accuracy[fold_idx] = calculate_accuracy(thresholds[best_threshold_index], dist[test_set],
-                                                      actual_issame[test_set])
-    return tprs, fprs, ppvs, accuracy
+            tprs[fold_idx, threshold_idx], fprs[fold_idx, threshold_idx], _, _ = calculate_accuracy(threshold, dist[test_set], actual_issame[test_set])
+        recall[fold_idx], _, precision[fold_idx], accuracy[fold_idx] = calculate_accuracy(thresholds[best_threshold_index], dist[test_set], actual_issame[test_set])
+
+    tpr = np.mean(tprs, 0)
+    fpr = np.mean(fprs, 0)
+
+    return tpr, fpr, recall, precision, accuracy
 
 
 def calculate_accuracy(threshold, dist, actual_issame):
@@ -526,7 +527,6 @@ def calculate_accuracy(threshold, dist, actual_issame):
     # https://en.wikipedia.org/wiki/Precision_and_recall
     # TPR = Recall
     tpr = 0 if (tp + fn == 0) else float(tp) / float(tp + fn)
-    # FPR = Fall-out
     fpr = 0 if (fp + tn == 0) else float(fp) / float(fp + tn)
     # Positive predictive value = Precision
     ppv = 0 if (tp + fp == 0) else float(tp) / float(tp + fp)

src/lfw.py
@@ -37,12 +37,12 @@ def evaluate(embeddings, actual_issame, nrof_folds=10):
     thresholds = np.arange(0, 4, 0.01)
     embeddings1 = embeddings[0::2]
     embeddings2 = embeddings[1::2]
-    tprs, fprs, ppvs, accuracy = facenet.calculate_roc(thresholds, embeddings1, embeddings2,
+    tpr, fpr, recall, precision, accuracy = facenet.calculate_roc(thresholds, embeddings1, embeddings2,
                                                np.asarray(actual_issame), nrof_folds=nrof_folds)
     thresholds = np.arange(0, 4, 0.001)
     val, val_std, far = facenet.calculate_val(thresholds, embeddings1, embeddings2,
                                               np.asarray(actual_issame), 1e-3, nrof_folds=nrof_folds)
-    return tprs, fprs, ppvs, accuracy, val, val_std, far
+    return tpr, fpr, recall, precision, accuracy, val, val_std, far
 
 
 def get_paths(lfw_dir, pairs, file_ext):

src/train_softmax.py
@@ -494,7 +494,7 @@ def evaluate(sess,
     assert np.array_equal(lab_array, np.arange(
         nrof_images)) == True, 'Wrong labels used for evaluation, possibly caused by training examples left in the input pipeline'
 
-    _, _, _, accuracy, val, val_std, far = lfw.evaluate(emb_array, actual_issame, nrof_folds=nrof_folds)
+    _, _, _, _, accuracy, val, val_std, far = lfw.evaluate(emb_array, actual_issame, nrof_folds=nrof_folds)
 
     print('Accuracy: %1.3f+-%1.3f' % (np.mean(accuracy), np.std(accuracy)))
     print('Validation rate: %2.5f+-%2.5f @ FAR=%2.5f' % (val, val_std, far))

src/validate_on_lfw.py
@@ -56,26 +56,27 @@ def main(args):
 
             # Load the model
             facenet.load_model(args.model)
+
             # TODO: replace near 0 parameters to 0
-            trainable_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)
+            # trainable_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)
 
-            print("trainable_vars", len(trainable_vars))
-            zero_threshold = 1e-2
-            n_var_elements = 0
-            n_non_zero = 0
-            n_non_zero_after = 0
-            assign_ops = []
-            for var in trainable_vars:
-                matrix = var.eval(sess)
-                # if var.name.endswith("weights:0"):
-                #     print(matrix)
-                n_var_elements += np.size(matrix)
-                n_non_zero += np.count_nonzero(matrix)
-                # matrix[np.abs(matrix)<=zero_threshold] = 0
-                # n_non_zero_after += np.count_nonzero(matrix)
-                # assign_ops.append(var.assign(matrix))
-
-            print("non_zero: ",n_non_zero,n_var_elements,n_non_zero/n_var_elements)
+            # print("trainable_vars", len(trainable_vars))
+            # zero_threshold = 1e-2
+            # n_var_elements = 0
+            # n_non_zero = 0
+            # n_non_zero_after = 0
+            # assign_ops = []
+            # for var in trainable_vars:
+            #     matrix = var.eval(sess)
+            #     # if var.name.endswith("weights:0"):
+            #     #     print(matrix)
+            #     n_var_elements += np.size(matrix)
+            #     n_non_zero += np.count_nonzero(matrix)
+            #     # matrix[np.abs(matrix)<=zero_threshold] = 0
+            #     # n_non_zero_after += np.count_nonzero(matrix)
+            #     # assign_ops.append(var.assign(matrix))
+            #
+            # print("non_zero: ",n_non_zero,n_var_elements,n_non_zero/n_var_elements)
             # print("non_zero after: ",n_non_zero_after,n_var_components,n_non_zero_after/n_var_components)
 
             # sess.run(assign_ops)
@@ -110,15 +111,11 @@ def main(args):
             duration = time.time() - start_time
             print('Forward pass duration in %.3f seconds' % duration)
 
-            tprs, fprs, ppvs, accuracy, val, val_std, far = lfw.evaluate(emb_array, actual_issame, nrof_folds=args.lfw_nrof_folds)
-
-            tpr = np.mean(tprs, 0)
-            fpr = np.mean(fprs, 0)
-            ppv = np.mean(ppvs, 0)
+            tpr, fpr, recall, precision, accuracy, val, val_std, far = lfw.evaluate(emb_array, actual_issame, nrof_folds=args.lfw_nrof_folds)
 
             print('Accuracy: %1.3f+-%1.3f' % (np.mean(accuracy), np.std(accuracy)))
-            print('Precision: %1.3f+-%1.3f' % (ppv, np.std(ppvs)))
-            print('Recall: %1.3f+-%1.3f' % (tpr, np.std(tprs)))
+            print('Precision: %1.3f+-%1.3f' % (np.mean(precision), np.std(precision)))
+            print('Recall: %1.3f+-%1.3f' % (np.mean(recall), np.std(recall)))
             print('Validation rate: %2.5f+-%2.5f @ FAR=%2.5f' % (val, val_std, far))
 
             auc = metrics.auc(fpr, tpr)

tmp/test_invariance_on_lfw.py
@@ -154,7 +154,7 @@ def evaluate_accuracy(sess, images_placeholder, phase_train_placeholder, image_s
     thresholds = np.arange(0, 4, 0.01)
     embeddings1 = emb_array[0::2]
     embeddings2 = emb_array[1::2]
-    _, _, _, accuracy = facenet.calculate_roc(thresholds, embeddings1, embeddings2, np.asarray(actual_issame), seed)
+    _, _, _, _, accuracy = facenet.calculate_roc(thresholds, embeddings1, embeddings2, np.asarray(actual_issame), seed)
     return accuracy
 
 def scale_images(images, scale, image_size):